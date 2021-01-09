82c9694dfbeab4a94795080bc82a509c2b5a7a87
Malena Morgensen <puzhengcscscscs@gmail.com>
2021-01-10 02:33:08 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# SteamKeyGenerator
-Fake Steam Key Generator PHP
+# Fake Steam Key Generator
+
+## Use
+```
+php SteamKeyGenerator.php -a 100
+```
+Where ***100*** is amount of keys to generate.
+
+The keys are saved to the **keys.txt** file which is in the same folder.
---
+++ SteamKeyGenerator_default.php
@@ -0,0 +1,44 @@
+<?php
+function gen() {
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString1 = '';
+    $randomString2 = '';
+    $randomString3 = '';
+    $randomString4 = '';
+    $randomString5 = '';
+    for ($i = 0; $i < 5; $i++) {
+            $randomString1 .= $characters[rand(0, $charactersLength - 1)];
+    }
+    for ($i = 0; $i < 5; $i++) {
+            $randomString2 .= $characters[rand(0, $charactersLength - 1)];
+    }
+    for ($i = 0; $i < 5; $i++) {
+            $randomString3 .= $characters[rand(0, $charactersLength - 1)];
+    }
+    for ($i = 0; $i < 5; $i++) {
+            $randomString4 .= $characters[rand(0, $charactersLength - 1)];
+    }
+    for ($i = 0; $i < 5; $i++) {
+            $randomString5 .= $characters[rand(0, $charactersLength - 1)];
+    }
+    $key = $randomString1 . "-" . $randomString2 . "-" . $randomString3 . "-" . $randomString4 . "-" . $randomString5;
+
+    echo $key . PHP_EOL;
+    return $key;
+}
+
+$options = getopt("a:"); // Required value (-a)
+
+$a = $options['a'];
+if (is_numeric($a) && $a > 0) {
+    while ($a > 0) {
+        $save[] = gen();
+        $a--;
+    }
+} else {
+    echo "Enter the amount.";
+}
+
+$saveFile = "keys.txt";
+file_put_contents($saveFile, implode("\n", $save));
